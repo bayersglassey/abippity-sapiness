a807c644f3ba76b068efaffc0ad53bcc6c526272
Ben Ayers-Glassey <bayersglassey@gmail.com>
2019-01-04 08:32:07 -0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Abippity',
+    version = '1.0.0',
+    author = 'B Ayers-Glassey',
+    author_email = 'bayersglassey@gmail.com’,
+    description = 'Reverse engineered implementation of ABAP in Python',
+    packages = find_packages(),
+)
