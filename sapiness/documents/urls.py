c5233bbb0dcfaa86a3782f9540800889800cccad
Ben Ayers-Glassey <bayersglassey@gmail.com>
2019-01-05 02:29:59 -0800

    sapiness: added args_help, keyword_help pages, expanded existing templates. Can pass abipitty args on document detail page.

--- sapiness/documents/urls.py
+++ sapiness/documents/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import url, include
 
 from . import views
 
+
 urlpatterns = [
     url(r'^list/?$', views.DocumentListView.as_view(), name='list'),
     url(r'^detail/(?P<pk>[0-9]+)/?$', views.DocumentDetailView.as_view(), name='detail'),
+    url(r'^args_help/?$', views.ArgsHelpView.as_view(), name='args_help'),
+    url(r'^keywords_help/?$', views.KeywordsHelpView.as_view(), name='keywords_help'),
 ]
--- sapiness/documents/views.py
+++ sapiness/documents/views.py
@@ -2,10 +2,27 @@
 import io
 
 from django.views.generic import ListView, DetailView
+from django.views.generic import TemplateView
 
-from abipitty.main import main
+from abipitty.main import main, parse_options, list_args
+from abipitty.lib.parse import get_keywords_text
 
 from .models import Document
+
+
+class ArgsHelpView(TemplateView):
+    template_name = 'documents/args_help.html'
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['args'] = list_args
+        return context
+
+class KeywordsHelpView(TemplateView):
+    template_name = 'documents/keywords_help.html'
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['keywords_dump'] = get_keywords_text()
+        return context
 
 
 class DocumentListView(ListView):
@@ -15,26 +32,47 @@ class DocumentListView(ListView):
 class DocumentDetailView(DetailView):
     model = Document
     template_name = 'documents/detail.html'
-    def post(self, request, *args, **kwargs):
+
+    def get_context_data(self, object):
+        context = super().get_context_data(object=object)
+        context['args'] = '--run --report'
+        return context
+
+    def dispatch(self, request, *args, **kwargs):
         # From DetailView.get:
         doc = self.object = self.get_object()
         context = self.get_context_data(object=self.object)
 
+        # User clicks "RUN":
         if 'run' in request.POST:
-            # Hack, abipitty.main just uses print statements with file=file,
-            # we psas it a StringIO as file in order to get text we can
-            # stuff into the response...
-            output_io = io.StringIO()
+            # Get args from POST, parse as abipitty options
+            args_text = request.POST.get('args', '')
+            args = args_text.split()
+            options = parse_options(args)
 
-            # Run abipitty on the document's content:
-            options = dict(RUN=True, PRINT_REPORT=True)
-            main(doc.content, options, file=output_io)
+            # Run abippity interpreter on document's content
+            output = self.run(doc.content, options)
 
-            # Read the output of abipitty.main, stick it in the context to
-            # be rendered
-            output_io.seek(0)
-            output = output_io.read()
+            context['args'] = args_text
             context['output'] = output
 
         # From DetailView.get:
         return self.render_to_response(context)
+
+    def run(self, text, options):
+        """Wrapper around abippity.main, returning its output as a str"""
+
+        # Hack, abipitty.main just uses print statements with file=file,
+        # we pass it a StringIO as file in order to get text we can
+        # stuff into the response...
+        output_io = io.StringIO()
+
+        # Run abipitty interpreter
+        main(text, options, file=output_io)
+
+        # Read the output of abipitty.main, stick it in the context to
+        # be rendered
+        output_io.seek(0)
+        output = output_io.read()
+
+        return output
