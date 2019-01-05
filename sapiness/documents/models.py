7ff816d1e186befee4e29dda425118f73a38099c
Ben Ayers-Glassey <bayersglassey@gmail.com>
2019-01-04 22:54:36 -0800

    documents: added Document fields user, public_{read,write} (migration included)

--- sapiness/documents/models.py
+++ sapiness/documents/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 class Document(models.Model):
+
     title = models.CharField("Title", max_length=200)
     content = models.TextField("Content")
+    user = models.ForeignKey(User, verbose_name="User",
+        null=True, blank=True)
+    public_read = models.BooleanField("Public (Readable)", default=False)
+    public_write = models.BooleanField("Public (Writable)", default=False)
+
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return "<Document {}: {}>".format(self.id, self)
+
