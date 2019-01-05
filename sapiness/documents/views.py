c3e048f982d24d72b97589376e9f35209a7771cb
Ben Ayers-Glassey <bayersglassey@gmail.com>
2019-01-05 00:57:48 -0800

    sapiness: DocumentListView calls abipitty.main on POST

--- sapiness/documents/views.py
+++ sapiness/documents/views.py
@@ -1,5 +1,9 @@
 
+import io
+
 from django.views.generic import ListView, DetailView
+
+from abipitty.main import main
 
 from .models import Document
 
@@ -11,3 +15,26 @@ class DocumentListView(ListView):
 class DocumentDetailView(DetailView):
     model = Document
     template_name = 'documents/detail.html'
+    def post(self, request, *args, **kwargs):
+        # From DetailView.get:
+        doc = self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+
+        if 'run' in request.POST:
+            # Hack, abipitty.main just uses print statements with file=file,
+            # we psas it a StringIO as file in order to get text we can
+            # stuff into the response...
+            output_io = io.StringIO()
+
+            # Run abipitty on the document's content:
+            options = dict(RUN=True, PRINT_REPORT=True)
+            main(doc.content, options, file=output_io)
+
+            # Read the output of abipitty.main, stick it in the context to
+            # be rendered
+            output_io.seek(0)
+            output = output_io.read()
+            context['output'] = output
+
+        # From DetailView.get:
+        return self.render_to_response(context)
