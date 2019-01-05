6ba88bdb2e0edfc6589f3e7dde97d2aa4a0981ca
Ben Ayers-Glassey <bayersglassey@gmail.com>
2019-01-05 03:29:29 -0800

    sapiness: update_example_docs: the docs are owned by admin user

--- sapiness/documents/utils.py
+++ sapiness/documents/utils.py
@@ -2,14 +2,22 @@
 import os, glob
 
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
 from .models import Document
 
+User = get_user_model()
 
-def create_doc_from_filepath(filepath, unique=True, **kwargs):
+
+def create_doc_from_filepath(filepath, unique=False, **kwargs):
     with open(filepath) as f: content = f.read()
     title = os.path.basename(filepath)
-    if unique: Document.objects.filter(title=title).delete()
+    if unique:
+        docs = Document.objects.filter(title=title)
+        if 'user' in kwargs:
+            user = kwargs['user']
+            docs = docs.filter(user=user)
+        docs.delete()
     doc = Document.objects.create(title=title, content=content,
         **kwargs)
     return doc
@@ -25,6 +33,14 @@ def create_docs_from_glob(fileglob, **kwargs):
 def update_example_docs(**kwargs):
     """Only works from within the git repo currently containing abippity
     and sapiness - the example files live with abippity."""
+
+    if 'user' not in kwargs:
+        # hardcoded username, good times
+        user = User.objects.filter(username='admin').first()
+        kwargs['user'] = user
+
+    if 'unique' not in kwargs: kwargs['unique'] = True
+
     fileglob = os.path.join(settings.BASE_DIR, os.path.pardir,
         'abippity', 'examples', '*.ab')
     return create_docs_from_glob(fileglob, **kwargs)
