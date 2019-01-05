2305bb4232e101fb7fb6a28a1d53e8de598570ad
Ben Ayers-Glassey <bayersglassey@gmail.com>
2019-01-04 23:24:07 -0800

    main.py: factored main, parse_options out of "if __name__ == '__main__': ..."

--- main.py
+++ main.py
@@ -1,88 +1,119 @@
 #!/bin/env python
 
-from sys import stdin, stdout, argv
 from lib.lex import lex, to_stmts
 from lib.parse import (get_keywords, print_syntax, parse, group,
     print_grouped_stmts)
 from lib.run import Runner
 
-if __name__ == '__main__':
-    text = stdin.read()
-    args = argv[1:]
-
-    PRINT_KEYWORDS = '--keywords' in args
+OPTIONS = [
+    # general
+    ('PRINT_KEYWORDS', '--keywords'),
 
     # lexing arguments
-    LEX_VERBOSE = '--lex-verbose' in args
-    LEX_SYNTAX = '--lex-syntax' in args
-    PRINT_LEXEMES = '--lexemes' in args
-    PRINT_STMTS = '--stmts' in args
+    ('LEX', '--lex'),
+    ('LEX_VERBOSE', '--lex-verbose'),
+    ('LEX_SYNTAX', '--lex-syntax'),
+    ('PRINT_LEXEMES', '--lexemes'),
+    ('PRINT_STMTS', '--stmts'),
 
     # parsing options
-    PARSE = '--parse' in args
-    PARSE_VERBOSE = '--parse-verbose' in args
-    PRINT_PARSED_STMTS = '--parsed-stmts' in args
+    ('PARSE', '--parse'),
+    ('PARSE_VERBOSE', '--parse-verbose'),
+    ('PRINT_PARSED_STMTS', '--parsed-stmts'),
 
     # grouping options
-    GROUP = '--group' in args
-    GROUP_VERBOSE = '--group-verbose' in args
-    PRINT_GROUPED_STMTS = '--grouped-stmts' in args
+    ('GROUP', '--group'),
+    ('GROUP_VERBOSE', '--group-verbose'),
+    ('PRINT_GROUPED_STMTS', '--grouped-stmts'),
 
     # running arguments
-    RUN = '--run' in args
-    RUN_VERBOSE = '--run-verbose' in args
-    PRINT_REPORT = '--report' in args
-    PRINT_VARS = '--vars' in args
-    VERBOSE_BOOLS = '--verbose-bools' in args
+    ('RUN', '--run'),
+    ('RUN_VERBOSE', '--run-verbose'),
+    ('PRINT_REPORT', '--report'),
+    ('PRINT_VARS', '--vars'),
+    ('VERBOSE_BOOLS', '--verbose-bools'),
+]
+
+
+
+
+def parse_options(args):
+
+    # basic parsing
+    options = {}
+    for name, arg in OPTIONS:
+        options[name] = arg in args
+
+    # option chaining
+    if options['RUN']:
+        options['GROUP'] = True
+    if options['GROUP']:
+        options['PARSE'] = True
+    if options['PARSE']:
+        options['LEX'] = True
+
+    return options
+
+
+def main(text, options, keywords=None):
 
     # get keywords
-    keywords = get_keywords()
-    if PRINT_KEYWORDS:
+    if keywords is None: keywords = get_keywords()
+    if options['PRINT_KEYWORDS']:
         print("KEYWORDS:")
         for keyword, syntax in keywords.items():
             print()
             print("{}:".format(keyword))
             print_syntax(syntax, 1)
 
-    # lex text into lexemes
-    lexemes = lex(text, verbose=LEX_VERBOSE, syntax=LEX_SYNTAX)
-    if PRINT_LEXEMES:
-        for lexeme in lexemes:
-            print(lexeme)
+    # lex text into stmts
+    if options['LEX']:
+        # lex text into lexemes
+        lexemes = lex(text, verbose=options['LEX_VERBOSE'],
+            syntax=options['LEX_SYNTAX'])
+        if options['PRINT_LEXEMES']:
+            for lexeme in lexemes:
+                print(lexeme)
 
-    # get stmts from lexemes
-    stmts = to_stmts(lexemes)
-    if PRINT_STMTS:
-        for stmt in stmts:
-            print(stmt)
+        # get stmts from lexemes
+        stmts = to_stmts(lexemes)
+        if options['PRINT_STMTS']:
+            for stmt in stmts:
+                print(stmt)
 
     # parse & group & run
-    if RUN:
-        GROUP = True
-    if GROUP:
-        PARSE = True
-    if PARSE:
+    if options['PARSE']:
         # parse stmts
-        parsed_stmts = parse(stmts, verbose=PARSE_VERBOSE)
-        if PRINT_PARSED_STMTS:
+        parsed_stmts = parse(stmts, verbose=options['PARSE_VERBOSE'])
+        if options['PRINT_PARSED_STMTS']:
             print("PARSED STATEMENTS:")
             for parsed_stmt in parsed_stmts:
                 print(parsed_stmt)
 
-        if GROUP:
+        if options['GROUP']:
             # group stmts
-            grouped_stmts = group(parsed_stmts, verbose=GROUP_VERBOSE)
-            if PRINT_GROUPED_STMTS:
+            grouped_stmts = group(parsed_stmts,
+                verbose=options['GROUP_VERBOSE'])
+            if options['PRINT_GROUPED_STMTS']:
                 print("GROUPED STATEMENTS:")
                 print_grouped_stmts(grouped_stmts, 1)
 
             # run grouped stmts
-            if RUN:
-                runner = Runner(40, 20, verbose=RUN_VERBOSE,
-                    verbose_bools=VERBOSE_BOOLS)
+            if options['RUN']:
+                runner = Runner(40, 20, verbose=options['RUN_VERBOSE'],
+                    verbose_bools=options['VERBOSE_BOOLS'])
                 report = runner.run(grouped_stmts, toplevel=True)
-                if PRINT_REPORT:
+                if options['PRINT_REPORT']:
                     report.print()
-                if PRINT_VARS:
-                    for varname, value in runner.vars.items():
-                        print("{}".format(value))
+                if options['PRINT_VARS']:
+                    for var in runner.vars.values():
+                        print("{}".format(var))
+
+
+
+if __name__ == '__main__':
+    from sys import stdin, stdout, argv
+    text = stdin.read()
+    args = argv[1:]
+    options = parse_options(args)
+    main(text, options)
